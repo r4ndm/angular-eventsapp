99524758c8e934a28ea8d1d49180975320e8eff6
Ranadeep Mukherjee <muuher@yahoo.com>
2019-06-23 11:56:40 -0500

    Add event service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/event.service';
 
 @NgModule({
   imports: [
@@ -16,7 +17,9 @@ import { NavBarComponent } from './nav/navbar.component';
     EventThumbnailComponent,
     NavBarComponent
   ],
-  providers: [],
+  providers: [
+    EventService
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
